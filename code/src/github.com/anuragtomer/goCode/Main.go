e241f3b457878908526f870e12300fcf8717f729
Anurag Tomer <anuragtomer@github.com>
2021-11-12 23:09:16 +0530

    Initial Commit

---
+++ code/src/github.com/anuragtomer/goCode/Main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var j int
+	j = 18
+	fmt.Println(j)
+	var k int = 19
+	fmt.Println(k)
+	i := 42
+	fmt.Println(i)
+	fmt.Println("Hello everyone")
+	fmt.Println("Testing")
+}
+
+// go run <filename>
